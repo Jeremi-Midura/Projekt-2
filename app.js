440e761f4bf55cdae90c8f61d86e75838283bd82
Jeremi-Midura <122528804+Jeremi-Midura@users.noreply.github.com>
2025-06-16 14:38:16 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ console.log(':)');
 document.addEventListener("keydown", (event) => {
     if (event.key === "ArrowDown") {
         window.scrollBy({ top: 50, behavior: "smooth" });
-        event.preventDefault();
     } else if (event.key === "ArrowUp") {
         window.scrollBy({ top: -50, behavior: "smooth" });
-        event.preventDefault();
     }
 });
+
